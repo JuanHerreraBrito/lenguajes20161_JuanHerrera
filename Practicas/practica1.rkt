39ae934763bcd954a88e7243a4497620c0fd71eb
JuanHerreraBrito <fenix.gnf19@gmail.com>
2015-08-19 15:51:52 -0500

    Ejercicio 1

--- Practicas/practica1.rkt
+++ Practicas/practica1.rkt
@@ -1,1 +1,29 @@
 #lang plai
+
+(print-only-errors true)
+
+;Funcion pow 
+(define (pow x y)
+ (cond
+   [(zero? y) 1]
+   [else (* x (pow x (- y 1)))]))
+
+;Funcion average
+
+;Funcion primes
+
+;Funcion zip
+
+;Funcion reduce
+
+;Funcion mconcat
+
+;Funcion mmap
+
+;Funcion mfilter
+
+;Funcion any?
+
+;Funcion every?
+
+;Funcion mpowerset
