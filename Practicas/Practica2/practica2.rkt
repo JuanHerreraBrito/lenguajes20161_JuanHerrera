bd6deb95746d5591c7744f237c205962c48baa30
marcobravociencias <marcobravo@ciencias.unam.mx>
2015-09-04 18:35:39 -0500

    idea del albol enario, ya me voy a casa

--- Practicas/Practica2/practica2.rkt
+++ Practicas/Practica2/practica2.rkt
@@ -3,11 +3,16 @@
 (define-type Array
   [MArray (n number?) (lst list?)])
 
-(test (MArray 5 '(1 2 3 4 5)) (MArray 5 '(1 2 3 4 5)) )
+;(test (MArray 5 '(1 2 3 4 5)) (MArray 5 '(1 2 3 4 5)) )
 
 (define-type MList
   [MEmpty]
   [MCons (n number?) (l MList?)])
 
-(test (MEmpty) (MEmpty))
-(test (MCons 1(MCons 2 (MCons 3 (MEmpty)))) (MCons 1(MCons 2 (MCons 3 (MEmpty)))) )
+;(test (MEmpty) (MEmpty))
+;(test (MCons 1(MCons 2 (MCons 3 (MEmpty)))) (MCons 1(MCons 2 (MCons 3 (MEmpty)))) )
+
+(define-type NTree
+  [TLEmpty]
+  ;[NodeN lista de tlemptys que se llama nnodo o una lista e cosas que se llama nnoden
+  )
