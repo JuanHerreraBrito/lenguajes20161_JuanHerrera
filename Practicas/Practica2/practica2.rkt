a68fe998fb4414b2d1b0488bae82db39241c95cf
JuanHerreraBrito <fenix.gnf19@gmail.com>
2015-09-05 00:12:26 -0500

    huesitos practica2, terminadas definiciones sin NTree

--- Practicas/Practica2/practica2.rkt
+++ Practicas/Practica2/practica2.rkt
@@ -1,13 +1,92 @@
 #lang plai
 
+
+;Seccion 1
+
+;1 MArray
 (define-type Array
-  [MArray (n number?) (lst list?)])
+  [MArray (n any/c) (lst list?)])
 
 (test (MArray 5 '(1 2 3 4 5)) (MArray 5 '(1 2 3 4 5)) )
 
+;2 MList
 (define-type MList
   [MEmpty]
-  [MCons (n number?) (l MList?)])
+  [MCons (n any/c) (l MList?)]);;poner any en lugar de number
 
 (test (MEmpty) (MEmpty))
 (test (MCons 1(MCons 2 (MCons 3 (MEmpty)))) (MCons 1(MCons 2 (MCons 3 (MEmpty)))) )
+
+
+;3 NTree
+#|(define-type NTree
+  [TLEmpty]
+  [NodeN (n any/c) ((ListOfNtree? l) List?)])
+
+(define (ListOfNTree? lst)
+  (cond
+    [(empty?) #t]
+    [((car lst) NTree?)]
+   ))
+
+|#
+
+;4 Position
+ (define-type Position
+   [2D-Point (n number?) (m number?)])  
+
+ (test (2D-Point 422 4545) (2D-Point 422 4545))
+
+;5 Figure
+(define-type Figure
+             [Circle (p 2D-Point?) (n number?)]
+             [Square (p 2D-Point?) (n number?)]
+             [Rectangle (p 2D-Point?) (n number?) (m number?)])
+
+(test (Circle (2D-Point 422 4545) 5) (Circle (2D-Point 422 4545) 5))
+(test (Square (2D-Point 422 4545) 5) (Square (2D-Point 422 4545) 5))
+(test (Rectangle (2D-Point 422 4545) 5 6) (Rectangle (2D-Point 422 4545) 5 6))
+
+;Seccion 2: Funciones sobre datos
+
+;6 setvalueA
+
+
+
+;7 MArray2MList
+
+
+;8 printML
+
+
+;9 concatML
+
+
+;10 lengthML
+
+
+;11 mapML
+
+
+;12 filterML
+
+
+;13 haversine
+
+
+;14 gps-coordinates
+
+
+;15 closest-building
+
+
+;16 building-at-distance
+
+
+;17 area
+
+
+;18 in-figure?
+
+
+
