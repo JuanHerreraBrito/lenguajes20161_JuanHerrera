2381e35499abea41af912fee98a82cc3a2a31ff2
marcobravociencias <marcobravo@ciencias.unam.mx>
2015-11-17 12:24:52 -0600

    Clase Vertex

---
+++ Practicas/Practica6/Vertex.py
@@ -0,0 +1,14 @@
+class Vertex(object):
+	"""docstring for Vertex"""
+	def __init__(self, inicio, fin, grado):
+		super(Vertex, self).__init__()
+		self.neighbours[]
+		self.inicio= inicio
+		self.fin= fin
+		self.grado= grado
+		"""Metodo que regresa los vertices adyacentes"""
+	def neighbours(self):
+			return self.neighbours
+		"""Metodo que regresa el grado del vertice"""
+	def degree(self):
+			return self.grado
