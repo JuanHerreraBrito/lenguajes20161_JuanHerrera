89afe1db9dd0c699fac7929932a4e0582534648a
JuanHerreraBrito <fenix.gnf19@gmail.com>
2015-11-19 02:04:58 -0600

    ya hecho clase

--- Practicas/Practica6/GraphReader.py
+++ Practicas/Practica6/GraphReader.py
@@ -2,22 +2,26 @@ from sys import argv
 
 script, nombreArchivo = argv
 
-""" Para json, se debe meter en metodo y hacer clase en general """
-jsonFile = open(nombreArchivo, 'r')
+class GraphReader(object):
 
-s = jsonFile.read();
+    def __init__(self, nombre = None):
+        self.nombre = nombre
 
-arr = s.split(',', 1)
-""" cuidar caso con espacio y sin espacio"""
-"""print arr[0].split(':', 1)[1]"""
-""" bueno en el caso actual """
-print arr[0].split(': ', 1)[1] 
+    def read_file(self, name):
+        """ Para json, se debe meter en metodo y hacer clase en general """
+        jsonFile = open(name, 'r')
+        s = jsonFile.read()
+        arr = s.split(',', 1)
+        """ cuidar caso con espacio y sin espacio"""
+        """print arr[0].split(':', 1)[1]"""
+        """ bueno en el caso actual """
+        print arr[0].split(': ', 1)[1]
+        vertices = arr[1].split('[')[1]
+        print vertices.split(']')[0]
+        for x in range(3, arr[1].split('[').__len__() - 1):
+            print arr[1].split('[')[x].split(']')[0]
 
-vertices = arr[1].split('[')[1]
+        jsonFile.close()
 
-print vertices.split(']')[0]
-
-for x in range(3, arr[1].split('[').__len__() - 1):
-	print arr[1].split('[')[x].split(']')[0]
-
-jsonFile.close()
+cosa = GraphReader()
+cosa.read_file(nombreArchivo)
