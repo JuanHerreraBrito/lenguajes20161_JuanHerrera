e707cbed256fccbe38698802d324f12ded9c5afb
JuanHerreraBrito <fenix.gnf19@gmail.com>
2015-11-19 04:00:49 -0600

    Listos datos limpio en GraphReader para su uso con todo y metodos

--- Practicas/Practica6/GraphReader.py
+++ Practicas/Practica6/GraphReader.py
@@ -3,6 +3,10 @@ from sys import argv
 script, nombreArchivo = argv
 
 class GraphReader(object):
+    
+    directed = -1
+    vertices = []
+    aristas = []
 
     def __init__(self, nombre = None):
         self.nombre = nombre
@@ -15,13 +19,33 @@ class GraphReader(object):
         """ cuidar caso con espacio y sin espacio"""
         """print arr[0].split(':', 1)[1]"""
         """ bueno en el caso actual """
-        print arr[0].split(': ', 1)[1]
-        vertices = arr[1].split('[')[1]
-        print vertices.split(']')[0]
-        for x in range(3, arr[1].split('[').__len__() - 1):
-            print arr[1].split('[')[x].split(']')[0]
+        self.directed = int(arr[0].split(': ', 1)[1])
+        verticesSucio = arr[1].split('[')[1]
+        self.vertices = verticesSucio.split(']')[0].split(', ')
+
+        for i in range(0, self.vertices.__len__()):
+            self.vertices[i] = self.vertices[i].split('"')[1]
+
+        j = 0
+	self.aristas = [None]* (arr[1].split('[').__len__() - 3)
+        for x in range(3, arr[1].split('[').__len__()):
+            respaldo = arr[1].split('[')[x].split(']')[0].split(', ')
+            self.aristas[j] = [respaldo[0].split('"')[1],respaldo[1].split('"')[1],int(respaldo[2])]
+            j += 1 
 
         jsonFile.close()
 
+    def get_directed(self):
+        return self.directed
+
+    def get_vertices(self):
+        return self.vertices
+
+    def get_aristas(self):
+        return self.aristas
+
 cosa = GraphReader()
 cosa.read_file(nombreArchivo)
+print cosa.get_directed()
+print cosa.get_vertices()
+print cosa.get_aristas()
